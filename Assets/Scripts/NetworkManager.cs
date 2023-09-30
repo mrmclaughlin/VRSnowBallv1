0a29f8825baf99a28400b977169e5e515722925c
Thomas P McLaughlin <tom@mrmclaughlin.com>
2023-09-30 14:15:18 -0400

    added in Lobby system but not sure if it will work with VR

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+[System.Serializable]
+public class DefaultRoom
+{
+	public string Name;
+	public int sceneIndex;
+	public int maxPlayer;
+	
+	
+}
+
+
 
 public class NetworkManager : MonoBehaviourPunCallbacks
 {
+	public List<DefaultRoom> defaultRooms;
+	public GameObject roomUI;
+	public GameObject ConnectUI;
     // Start is called before the first frame update
     void Start()
     {
-       ConnectToServer(); 
+       //ConnectToServer(); 
     }
 
     // Update is called once per frame
@@ -18,7 +32,7 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         
     }
 	
-	void ConnectToServer(){
+	public void ConnectToServer(){
 		PhotonNetwork.ConnectUsingSettings();
 		Debug.Log("Yo!!! Try Connect to Server");
 		
@@ -27,16 +41,31 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 	{
 		Debug.Log("I am connected to Server");
 		base.OnConnectedToMaster();
-		RoomOptions roomOptions = new RoomOptions();
-		roomOptions.MaxPlayers = 10;
-		roomOptions.IsVisible = true;
-		roomOptions.IsOpen = true;
-		PhotonNetwork.JoinOrCreateRoom("Room 1",roomOptions,TypedLobby.Default);
+		PhotonNetwork.JoinLobby();
 	}
 	public override void OnJoinedRoom(){
 		Debug.Log("Yeah we are in a room");
 		base.OnJoinedRoom();
 		
+	}
+public override void OnJoinedLobby(){
+	base.OnJoinedLobby();
+	Debug.Log("We Joined the Lobby");
+	roomUI.gameObject.SetActive(true);
+	ConnectUI.gameObject.SetActive(false);
+	
+}
+	
+	public void InitiliazeRoom(int defaultRoomIndex){
+		DefaultRoom roomSettings = defaultRooms[defaultRoomIndex];
+		
+		//Load Scene
+		PhotonNetwork.LoadLevel(roomSettings.sceneIndex);
+		RoomOptions roomOptions = new RoomOptions();
+		roomOptions.MaxPlayers = (byte)roomSettings.maxPlayer;
+		roomOptions.IsVisible = true;
+		roomOptions.IsOpen = true;
+		PhotonNetwork.JoinOrCreateRoom(roomSettings.Name,roomOptions,TypedLobby.Default);
 	}
 	
 	
